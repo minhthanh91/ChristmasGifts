e03f8f51ad391c1859eeb9f5bdbf96d21932b29b
w3school-test <mingcheng357@gmail.com>
2021-11-11 17:05:27 +0700

    done

--- script.js
+++ script.js
@@ -1,4 +1,21 @@
-// $(document).ready(function () {
-//   //   wowJS
-//   new WOW().init();
-// });
+$(document).ready(function () {
+  // magnific-popup
+  $('.parent-container').magnificPopup({
+    // the containers for all your galleries
+    delegate: 'a', // the selector for gallery item
+    type: 'image',
+    gallery: {
+      enabled: true,
+    },
+  });
+
+  // back-to-top icon
+  $(document).scroll(function () {
+    let pos = $(this).scrollTop();
+    if (pos > 717) {
+      $('#back-to-top').addClass('scrollTop');
+    } else {
+      $('#back-to-top').removeClass('scrollTop');
+    }
+  });
+});
